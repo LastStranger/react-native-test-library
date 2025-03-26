109eef8680f90c42c73243d575ca784500b1ba70
laststranger <callmelz@qq.com>
2025-03-26 16:07:58 +0800

    feat: add initial version

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,14 @@
-import { multiply } from 'react-native-test-library';
+import { add, multiply } from 'react-native-test-library';
 import { Text, View, StyleSheet } from 'react-native';
 
 const result = multiply(3, 7);
+const res = add(3, 7);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Text>Result: {res}</Text>
     </View>
   );
 }
